61448b054cfa6ddb4d08478063fdb8eda6fa3c31
SEU_NOME_USUARIO_GITHUB <luiz.baptistella@onebox.one>
2026-06-01 16:43:09 -0300

    fix(ExpandableRadioItem): add missing RadioGroup import

--- src/components/blocks/ExpandableRadioItem.tsx
+++ src/components/blocks/ExpandableRadioItem.tsx
@@ -2,7 +2,7 @@ import * as React from "react"
 import { ChevronDown } from "lucide-react"
 
 import { cn } from "../../lib/utils"
-import { RadioGroupItem } from "../shadcn/RadioGroup"
+import { RadioGroup, RadioGroupItem } from "../shadcn/RadioGroup"
 import { Label } from "../shadcn/Label"
 
 const ExpandableRadioContext = React.createContext<string | undefined>(undefined)
